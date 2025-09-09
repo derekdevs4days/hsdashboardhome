50d3a4c23f41e948c319b90cdbfb3c1c82465f94
Derek Truong <derektruongdev@gmail.com>
2025-09-08 21:49:26 -0500

    change section name

--- src/components/DashboardGrid.tsx
+++ src/components/DashboardGrid.tsx
@@ -209,7 +209,7 @@ export function DashboardGrid() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3 text-[#E13F4E]" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
           </svg>
-          Marketing & Growth
+          Marketing
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
           {marketingDashboards.map(dashboard => <DashboardCard key={dashboard.id} dashboard={dashboard} />)}
